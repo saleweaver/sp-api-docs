6c008445ab3abb062d70bafb18d76946a359be80
Michael <info@saleweaver.com>
2021-11-29 07:55:39 +0100

    init

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ author = 'Michael Primke'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.googleanalytics',
     "sphinx_rtd_theme",
     'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
 
@@ -54,3 +55,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+googleanalytics_id = 'UA-158904000-4'
